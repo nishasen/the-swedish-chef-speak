db0bdda78cd9627e17e6af14fc229b33490e3dcf
nishasen <43262505+nishasen@users.noreply.github.com>
2021-09-11 22:14:25 +0530

    Completed

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-//var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
-var serverURL = "https://api.funtranslations.com/translate/minion.json"
+var serverURL = "https://api.funtranslations.com/translate/ubbi-dubbi.json"
 
 function getTranslationURL(text){
     return serverURL+"?"+"text="+text;
@@ -16,7 +15,7 @@ function getTranslationURL(text){
 
 function errorHandler(error){
     console.log("Error occured", error)
-    alert("Something wrong with API call and server, try again after sometimes")
+    alert("Something wrong with the server, try again after sometimes.")
 }
 
 btnTranslate.addEventListener("click", function clickEventHandler() {
